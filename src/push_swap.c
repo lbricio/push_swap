23c6d281600a54c5ec9121cd9c3e0ac719dd2e6f
lbricio <kirowhisky@gmail.com>
2021-10-15 16:04:07 -0300

    meu deus quase

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lbricio- <lbricio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 15:47:45 by lbricio-          #+#    #+#             */
-/*   Updated: 2021/10/15 15:46:20 by lbricio-         ###   ########.fr       */
+/*   Updated: 2021/10/15 16:03:48 by lbricio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static void	error(t_stack *a, t_stack *b, int i)
 	exit(1);
 }
 
-static void no_leaks(t_stack *a, t_stack *b)
+static void	no_leaks(t_stack *a, t_stack *b)
 {
 	free(a->array);
 	free(b->array);
@@ -57,7 +57,7 @@ int	main(int argc, char **argv)
 	if (check_sort(&a))
 	{
 		no_leaks(&a, &b);
-		return (0);	
+		return (0);
 	}
 	if (a.n <= 3)
 		micro_sort(&a, &b);
